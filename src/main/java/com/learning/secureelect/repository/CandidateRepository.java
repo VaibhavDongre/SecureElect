da7b2558803eca1ea7510e56da4b26d59f4220f1
Vaibhav Dongre <21vaibhavdongre@gmail.com>
2025-11-25 10:33:31 +0530

    Candidate service and Controller created

--- src/main/java/com/learning/secureelect/repository/CandidateRepository.java
+++ src/main/java/com/learning/secureelect/repository/CandidateRepository.java
@@ -3,9 +3,10 @@ package com.learning.secureelect.repository;
 import com.learning.secureelect.entity.Candidate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-    Optional<Candidate> findByElectionId(Long electionId);
+    List<Candidate> findByElectionId(Long electionId);
     Optional<Candidate> findByFaction(String faction);
 }
